ecf28f604ad9b2d5a1733cbb6d9d2fb0e80fc344
Ximawa <44783334+Ximawa@users.noreply.github.com>
2023-05-02 14:28:02 +0200

    rajout zone creation mot

--- index.php
+++ index.php
@@ -3,29 +3,24 @@
     require_once('./Structure_du_jeu/scrabble.php');
     require_once('./Structure_du_jeu/plateau.php');
 
+    session_start();
+
     $game = new Scrabble();
     $pioche = new Pioche();
     $toufik = new Joueur("toufik");
     $plateau = new Plateau();
 
     
-
-    // if(!isset($_SESSION['toufik'])){
-    //     $toufik->PiocheDebutPartie($pioche);
-    //     $_SESSION['toufik'] = $toufik;
-    //     var_dump($toufik->main);
-    // } 
-
     if(isset($_POST['pioche']) ){
         $toufik->PiocheDebutPartie($pioche);
         $_SESSION['toufik'] = $toufik;
     }
 
     if(isset($_POST['submit']) ){
-        $mot = $_POST['mot'];
+        $mot = $_POST['selection'];
         $toufik = $_SESSION['toufik'];
-        // var_dump($toufik->main);
-        // var_dump($mot);
+
+
         if($game->verifMotComposition($mot, $toufik->main) == true){
             echo "<h2>Mot bien composé de vos lettre </h2>";
             if($game->verifMotValide($mot) == true){
@@ -52,8 +47,7 @@
 </head>
 <body>
     <header>
-        <section >
-                
+        <section>         
                 <h1 ><img src="img\Logo_Epsi_Scrabble.png" alt="LOGO du jeu" >Scrabble</h1>
         </section>
         
@@ -87,10 +81,10 @@
     <section class ="container-sm">
         <div class ="row">
             <p class ="col">Nb de lettres restant dans la pioche : <?php echo $pioche->nombrePieces(); ?></p> 
-            <div class = "col">
+            <div class = "col" id="main">
                 <?php
                     foreach($toufik->main as $piece){
-                        echo $piece->lettre. " ";
+                        echo "<input type=\"button\" value=\"". $piece->lettre."\" onClick=\"jouer(this.value)\" id=\"". $piece->lettre."\"./>";
                     }
                 ?>
             </div>
@@ -99,14 +93,23 @@
             </form>
         </div>
        
-        <div>
-            <form method="post" action="">
-                <label for="mot">Enter a word:</label>
-                <input type="text" name="mot" id="mot">
+        <div >
+            <form name="fo" method="post" action="">
+                <input type="hidden" name="selection">
                 <input type="submit" name="submit" value="Check">
             </form>
         </div>
+        <div id="selection">
+
+        </div>
     </section>
+    <script>
+        function jouer(val) {
+            document.getElementById(val).style.visibility = "hidden";
+            document.getElementById("selection").innerHTML += '<input type="button" value="'+val+'">';
+            document.fo.selection.value+=val;
+        }
+    </script>
     
         <div>
             <img src="./img/Plateau" alt="Plateau de jeu du Scrabble">
@@ -115,7 +118,7 @@
     </main>
 
     <footer>
-        <b
+        
         <p>SCORE BOARD</p>
     </footer>  
     
