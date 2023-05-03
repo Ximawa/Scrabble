04bb1ef93c359e0e270971c3256c522b63fe6f64
Kerrigan <alban.joubert@gmail.com>
2023-05-03 13:24:18 +0200

    no message

--- Page_Joueur/pageHome.php
+++ Page_Joueur/pageHome.php
@@ -8,9 +8,48 @@
 </head>
 <body>
     <header>
-        <section>
-            
-        </section>
+        <img src="img\logo.png" alt="Logo">
+        <div>
+            <button>
+                Connexion
+            </button>
+            <button>
+                creaction de compte
+            </button>
+        </div>
     </header>
+
+    <main>
+        <section class="container mt-2">
+            <div class="text-white bg-dark p-2 rounded">
+                <div class="row">
+                    <p class="col">affichage de profil</p>
+
+                    <form method="post" action="" class="col">
+                        <input type="submit" name="Creaction de la partie" value="Creaction de la partie">
+                    </form>
+
+                    <form method="post" action="" class="col">
+                        <input type="submit" name="rejoindre la partie" value="rejoindre la partie">
+                    </form>
+                </div>
+                
+            </div>
+                
+        </section>
+
+    </main>
+
+    <footer>
+        <section>
+            <li><a href="">Contact</a></li>
+            <li><a href="">Conditions d'utilisation</a></li>
+            <li><a href="">Credits</a></li>
+            <li><a href="">Paramètres de confidentialité</a></li>
+        </section>
+        
+        <p>Le propriétaire de ce site n’est pas responsable du contenu généré par l’utilisateur (mot utiliser, messages, noms d’utilisateur)</p>
+    </footer>
+
 </body>
 </html>
