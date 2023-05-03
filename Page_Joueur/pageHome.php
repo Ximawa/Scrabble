43142ead8292e08420c48ec32c02228f17cf569f
Kerrigan <alban.joubert@gmail.com>
2023-05-03 13:38:29 +0200

    no message

--- Page_Joueur/pageHome.php
+++ Page_Joueur/pageHome.php
@@ -4,11 +4,13 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <link rel="stylesheet" href='.\affichage\styleHome.css'>
     <title>Document</title>
 </head>
 <body>
     <header>
-        <img src="img\logo.png" alt="Logo">
+        <img src="C:\xampp\htdocs\Projet Scrable Seb branch\img\Logo_Epsi_Scrabble.png" alt="logo">
         <div>
             <button>
                 Connexion
