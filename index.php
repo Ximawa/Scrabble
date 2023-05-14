86e39d675c700a7b933a4f5d8bf677f432f1be9b
Kerrigan <alban.joubert@gmail.com>
2023-05-14 18:44:15 +0200

    padding fait

--- index.php
+++ index.php
@@ -22,7 +22,7 @@
                 </a>
                 
             </div>
-            <div class="space">
+            <div class="space2">
             <div class="btn-toolbar" role="toolbar" aria-label="Toolbar with button groups">
                 <div class="btn-group me-2" role="group" aria-label="First group">
                     <a href="Page_Joueur\PageConnexion.php" class="btn btn-primary">Connexion</a>
