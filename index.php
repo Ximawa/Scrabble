fbc48217a1f4d04dab5e14a90be66e18b4edb85d
Kerrigan <alban.joubert@gmail.com>
2023-05-14 12:08:48 +0200

    no message

--- index.php
+++ index.php
@@ -11,16 +11,9 @@
 <body>
 <header>
         <img src="img\Logo_Epsi_Scrabble.png" alt="logo">
-        <div>
-            <button>
-                <a href="Page_Joueur\PageConnexion.php">Connexion</a>
-            </button>
-            
-            
-            <button>
-                <a href="Page_Joueur\PageInscription.php">Creaction de compte</a>
-                
-            </button>
+        <div class="btn-group btn-group-sm" role="group" aria-label="Small button group">
+                <a href="Page_Joueur\PageConnexion.php" class="btn btn-primary">Connexion</a>
+                <a href="Page_Joueur\PageInscription.php" class="btn btn-primary">Creaction de compte</a>
         </div>
 
     </header>
@@ -33,11 +26,11 @@
                     <p class="col">affichage de profil</p>
 
                     <form method="post" action="" class="col">
-                        <input type="submit" name="Creaction de la partie" value="Creaction de la partie">
+                        <input type="submit" name="Creaction de la partie" value="Creaction de la partie" class="btn btn-primary">
                     </form>
 
                     <form method="post" action="" class="col">
-                        <input type="submit" name="rejoindre la partie" value="rejoindre la partie">
+                        <input type="submit" name="rejoindre la partie" value="rejoindre la partie" class="btn btn-primary">
                     </form>
                 </div>
                 
@@ -48,8 +41,8 @@
             <div class="text-white bg-dark p-2 rounded">
                 <p>Local</p>
                 <div class="row">
-                    <form method="post" action="Structure_du_jeu/game.php" class="col">
-                        <input type="text" name="joueur1" placeholder="Nom joueur 1">
+                    <form method="post" action="Structure_du_jeu/game.php" class="col" class="form-label">
+                        <input type="text" name="joueur1" placeholder="Nom joueur 1" >
                     </form>
                     <form method="post" action="Structure_du_jeu/game.php"class="col">
                         <input type="text" name="joueur2" placeholder="Nom joueur 2">
@@ -59,16 +52,29 @@
                     
                 </div>
                 <div class="row">
-                    <form method="post" action="Structure_du_jeu/game.php">
-                         <input type="submit" name="start" value="Lancer Partie">
+                    <form method="post" action="Structure_du_jeu/game.php" >
+                         <input type="submit" name="start" value="Lancer Partie" class="btn btn-primary">
                     </form>
                 </div>    
             </div>  
-            
-            
-           
-        
         </section>
+        <br>
+        <br>
+
+        <p>
+        <button class="btn btn-primary" type="button" data-bs-toggle="collapse" data-bs-target="#collapseWidthExample" aria-expanded="false" aria-controls="collapseWidthExample">
+            Regles sccrabbles
+        </button>
+        </p>
+        <div style="min-height: 60px;">
+        <div class="collapse collapse-horizontal" id="collapseWidthExample">
+            <div class="card card-body" class="couleurtext" style="width: 300px;">
+                <div class="couleurtext">
+                    regles sccrabbles
+                </div>
+            </div>
+        </div>
+        </div>
     </main>
 
     <footer>
