571256150456f2c77f503c7658c4e32ebfd9e3b8
Ximawa <44783334+Ximawa@users.noreply.github.com>
2023-05-15 14:39:48 +0200

    ajout statistique page profil utilisateur

--- Page_Joueur/StatJoueur.php
+++ Page_Joueur/StatJoueur.php
@@ -10,11 +10,28 @@
     $stmt->execute();
     $resultats = $stmt->fetchAll();
 
+    $score = 0;
+    $nb_mot_total = 0;
+    $score_moyen = 0;
+    $score_moyen_mot = 0;
+    $nb_mot_moyen = 0;
+
     echo '<div> Historique de partie </div>';
     foreach($resultats as $resultat){
+        $nb_mot_total += $resultat["nb__mot_jouer"];
+        $score += $resultat["score"];
         echo "ID partie : " . $resultat["Id_partie"] . "<br>";
         echo "Score : " . $resultat["score"] . "<br>";
         echo "Nombre de mot jouer : " . $resultat["nb__mot_jouer"] . "<br>";
     }
+
+
+    echo '<br><div> Statistique du compte </div>';
+    echo 'Nb mot jouer au total : '.$nb_mot_total. "<br>";;
+    echo 'Score moyen par partie : '.($score / count($resultats)). "<br>";;
+    echo 'Score moyen par mot :'.($score / $nb_mot_total). "<br>";;
+    echo 'Moyenne de mot posé par partie : '.($nb_mot_total / count($resultats)). "<br>";;
+
+
 
 ?>
