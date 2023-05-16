e155881994a56836ea166242a058190947dec8b0
Ximawa <44783334+Ximawa@users.noreply.github.com>
2023-05-16 10:40:29 +0200

    modif script sql

--- projet_scrabble.sql
+++ projet_scrabble.sql
@@ -30,7 +30,7 @@ SET time_zone = "+00:00";
 CREATE TABLE `joueurs` (
   `Id` int(11) NOT NULL,
   `nom_joueur` varchar(50) NOT NULL,
-  `mdp` varchar(50) NOT NULL
+  `mdp` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
